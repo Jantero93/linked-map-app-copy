f694826d3a7b7f6f26255d4364b5ebcc0bd7a814
Jani Sillanpää <janzku1993@hotmail.com>
2024-05-05 11:51:52 +0300

    Censored authorization header in RequestLoggerMiddleware

--- MapServer/Middlewares/RequestLoggerMiddleware.cs
+++ MapServer/Middlewares/RequestLoggerMiddleware.cs
@@ -1,4 +1,6 @@
-﻿namespace MapServer.Middlewares;
+using Microsoft.Net.Http.Headers;
+
+namespace MapServer.Middlewares;
 
 public class RequestLoggerMiddleware(RequestDelegate next, ILogger<RequestLoggerMiddleware> logger)
 {
@@ -7,16 +9,21 @@ public class RequestLoggerMiddleware(RequestDelegate next, ILogger<RequestLogger
         var method = context.Request.Method;
         var path = context.Request.Path;
 
-        logger.LogDebug("Request Method: {RequestMethod}: {RequestPath}", method, path);
+        logger.LogDebug("Request Method: {RequestMethod}, Request Path: {RequestPath}", method, path);
 
         var queryParamsDict = context.Request.Query.ToDictionary(q => q.Key, q => q.Value.ToString());
-        if (queryParamsDict.Count is not 0)
+        if (queryParamsDict.Count > 0)
         {
             logger.LogDebug("Query Params: {@QueryParams}", queryParamsDict);
         }
 
         var headersDict = context.Request.Headers.ToDictionary(h => h.Key, h => h.Value.ToString());
-        if (headersDict.Count is not 0)
+        if (headersDict.ContainsKey(HeaderNames.Authorization))
+        {
+            headersDict[HeaderNames.Authorization] = "[redacted]";
+        }
+
+        if (headersDict.Count > 0)
         {
             logger.LogDebug("Headers: {@Headers}", headersDict);
         }
