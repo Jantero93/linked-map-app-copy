51b76d08051334085a0b9595d32c00b98d75f739
Jani Sillanpää <janzku1993@hotmail.com>
2024-06-02 06:43:46 +0300

    Add close icon on pop up location on map

--- react-client/src/hooks/mapHooks/useOverlay.ts
+++ react-client/src/hooks/mapHooks/useOverlay.ts
@@ -1,14 +1,16 @@
 import { useEffect, useRef, MutableRefObject } from "react";
 import { Overlay, Map } from "ol";
 
+type OverlayRef = MutableRefObject<HTMLDivElement | null>;
+
 const useOverlay = (
   mapInstanceRef: MutableRefObject<Map | null>,
-  popupRef: MutableRefObject<HTMLDivElement | null>
+  popupRef: OverlayRef
 ): MutableRefObject<Overlay | null> => {
   const overlayRef = useRef<Overlay | null>(null);
 
   useEffect(() => {
-    if (popupRef.current && mapInstanceRef.current) {
+    if (popupRef.current && mapInstanceRef.current && !overlayRef.current) {
       const overlay = new Overlay({
         element: popupRef.current,
         autoPan: true,
